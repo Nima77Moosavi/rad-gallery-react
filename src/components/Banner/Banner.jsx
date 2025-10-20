14278424cf0234a244d04dfc940c723ae679b821
fatemehghsmi <ghasemi12794@gmail.com>
2025-10-20 13:00:50 +0330

    BANNER

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Banner.module.css"
-import image1 from "../../assets/banner1.png";
-import image3 from "../../assets/img.png";
+// import image1 from "../../assets/banner1.png";
+// import image3 from "../../assets/img.png";
 
 
 const Banner = () => {
@@ -10,8 +10,8 @@ const Banner = () => {
 
   return (
     <div className={styles.container}>
-      <img src={image3} alt="" className={styles.dastImg}/>
-      <img src={image1} alt="" className={styles.textImg}/>
+      {/* <img src={image3} alt="" className={styles.dastImg}/>
+      <img src={image1} alt="" className={styles.textImg}/> */}
     </div>
   )
 }
